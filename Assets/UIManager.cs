252dc1627c3c3e57a898368f97b1d3554b536a90
iwaken71 <iwaken59371518@gmail.com>
2017-06-24 23:22:09 +0900

    写真追加

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : SingletonMonoBehaviour<UIManager> {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	[SerializeField]
+	Image image;
+
+	[SerializeField]
+	Text label;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	Sprite[] sprites;
+
+	public float smoothTime = 5f;
+
+
+
+
+
+	void Awake(){
+		sprites = Resources.LoadAll<Sprite>("Images");
 	}
+
+	void Start(){
+		SetImage();
+	}
+
+	public void SetImage(){
+		int index = Random.Range(0,sprites.Length);
+		image.sprite = sprites[index];
+		label.text = "injured";
+	}
+
+	void LateUpdate ()
+	{	
+		Vector3 pos = TransformToVector(Camera.main.transform,new Vector3(0,0,1.5f));
+		transform.position =pos;// Vector3.Lerp(transform.position,pos,Time.deltaTime*smoothTime);
+
+	}
+
+	Vector3 TransformToVector(Transform trans, Vector3 dir) {
+          return  trans.position + trans.forward * dir.z + trans.right * dir.x + trans.up * dir.y;
+    }
 }
