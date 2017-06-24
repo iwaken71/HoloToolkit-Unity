84f9590495a009acbca481db1822382118fad9b1
iwaken71 <iwaken59371518@gmail.com>
2017-06-24 11:18:15 +0900

    DemoBomb

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XRHack{
+	public class PlayerController : MonoBehaviour {
+		RaycastHit hit;
+		
+		// Use this for initialization
+		void Start () {
+				
+		}
+			
+			// Update is called once per frame
+		void Update ()
+		{
+			Ray ray = new Ray (transform.position, -transform.up);
+			if (Physics.SphereCast (ray, 0.05f ,out hit)) {
+				if (hit.collider.tag == "Bomb") {
+					hit.collider.GetComponent<BombController>().Explosion();
+				}
+			}
+
+		}
+	}
+}
