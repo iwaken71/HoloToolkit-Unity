424f1a0b1d71eb28290d48e416f5f97bada093f2
iwaken71 <iwaken59371518@gmail.com>
2017-06-25 10:55:35 +0900

    Floor削除

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,21 +17,24 @@ namespace XRHack {
         }
         // Use this for initialization
         void Start() {
-            //StartCoroutine(GenerateBombAround(20, 7));
+          
+            StartCoroutine(GenerateItems(20, 5));
             state = State.Ready;
+        
         }
 
         // Update is called once per frame
         void Update() {
             if (Input.GetMouseButtonDown(0)) {
-                StartCoroutine(GenerateBombAround(20,0));
+				StartCoroutine(GenerateItems(20,0));
             }
 
         }
 
-        public IEnumerator GenerateBombAround(int n,float timer) {
+        public IEnumerator GenerateItems(int n,float timer) {
+		
             yield return new WaitForSeconds(timer);
-            for (int i = 0; i < n; i++) {
+           /* for (int i = 0; i < n; i++) {
                 Vector3 pos = transform.position + new Vector3(Random.Range(-3.0f, 3.0f), -0.5f, Random.Range(1f, 5f));
                 int index = Random.Range(0,bombPrefabs.Length);
                 Instantiate(bombPrefabs[index], pos, Random.rotation);
@@ -39,18 +42,43 @@ namespace XRHack {
 				Vector3 pos2 = transform.position + new Vector3(Random.Range(-3.0f, 3.0f), -0.5f, Random.Range(1f, 5f));
                 int index2 = Random.Range(0,fruitPrefabs.Length);
                 Instantiate(fruitPrefabs[index2], pos, Random.rotation);
-            }
-
-			GenerateSoccerBall(5);
+            }*/
+			
+			GenerateSoccerBalls(5);
+			
 
         }
 
-        void GenerateSoccerBall (int n)
+        void GenerateBomb (int n,Vector3 center)
 		{
 			for (int i = 0; i < n; i++) {
-                Vector3 pos = transform.position + new Vector3(Random.Range(-3.0f, 3.0f), -0.5f, Random.Range(1f, 5f));
-                Instantiate(soccerBallPrefab, pos, Random.rotation);
+               float theta = Random.Range(0f,360f);
+               float r = Random.Range(1.0f,2.0f);
+				Vector3 pos = center + new Vector3(Mathf.Cos(theta*Mathf.PI/180),0,Mathf.Sin(theta*Mathf.PI/180))*r;
+				int index = Random.Range(0,bombPrefabs.Length);
+                Instantiate(bombPrefabs[index], pos, Random.rotation);
+            }
 
+		}
+		void GenerateFruit (int n,Vector3 center)
+		{
+			for (int i = 0; i < n; i++) {
+               float theta = Random.Range(0f,360f);
+               float r = Random.Range(1.0f,2.0f);
+				Vector3 pos = center + new Vector3(Mathf.Cos(theta*Mathf.PI/180),0,Mathf.Sin(theta*Mathf.PI/180))*r;
+				int index2 = Random.Range(0,fruitPrefabs.Length);
+                Instantiate(fruitPrefabs[index2], pos, Random.rotation);
+            }
+
+		}
+
+        void GenerateSoccerBalls (int n)
+		{
+			for (int i = 0; i < n; i++) {
+                Vector3 pos = Camera.main.transform.position + new Vector3(Random.Range(-3.0f, 3.0f), 0, Random.Range(1f, 5f));
+                Instantiate(soccerBallPrefab, pos, Random.rotation);
+				GenerateBomb(5,pos);
+				GenerateFruit(10,pos);
             }
 		}
 
